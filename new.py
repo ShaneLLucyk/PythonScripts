01093faa9e83e24c90d6f5898252a3112b3a3938
ShaneLLucyk <shane-lucyk@hotmail.com>
2018-10-30 13:24:22 -0700

    Adding Prettify 2 again

--- new.py
+++ new.py
@@ -5,3 +5,4 @@ from bs4 import BeautifulSoup
 page = requests.get('https://www.imdb.com/title/tt1270797/?ref_=fn_al_tt_1')
 soup = BeautifulSoup(page.text, 'html.parser')
 print(soup.prettify())
+print(soup.prettify())
