4a0bb1612edd01131be991dffc3936ac6a57a780
ShaneLLucyk <shane-lucyk@hotmail.com>
2018-10-30 13:18:39 -0700

    Removing prettify_2

--- new.py
+++ new.py
@@ -5,4 +5,3 @@ from bs4 import BeautifulSoup
 page = requests.get('https://www.imdb.com/title/tt1270797/?ref_=fn_al_tt_1')
 soup = BeautifulSoup(page.text, 'html.parser')
 print(soup.prettify())
-print(soup.prettify())
