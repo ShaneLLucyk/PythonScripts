cc3a4cb3dda3021ffbfbf04e199ebb74fff87307
ShaneLLucyk <shane-lucyk@hotmail.com>
2018-10-30 12:59:59 -0700

    Initual Upload

---
+++ new.py
@@ -0,0 +1,7 @@
+#Import Libraries
+import requests
+from bs4 import BeautifulSoup
+
+page = requests.get('https://www.imdb.com/title/tt1270797/?ref_=fn_al_tt_1')
+soup = BeautifulSoup(page.text, 'html.parser')
+print(soup.prettify())
